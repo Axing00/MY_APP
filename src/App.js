dee09cb305441a22e2d3b0512650b6166f505277
ellie <gyeongjin.hong@dailyh.org>
2023-05-04 10:24:09 +0900

    + 연습

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // javascript 경우
+  // document.querySelector('h4').innerHTML = post;
+  let post = '강남 우동 맛집';
+  // 데이터바인등은{중괄호}
+
+  // style을 넣을때는 style={{스타일명 : '값'}}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={{ color: 'red', fontSzie: '16px' }}>블로그임</h4>
+      </div>
+
+      <h4>{post}</h4>
     </div>
   );
 }
