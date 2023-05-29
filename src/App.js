3301fb32350e1007d0e884ac94196213343b180e
ellie <gyeongjin.hong@dailyh.org>
2023-05-30 00:12:42 +0900

    + map 함수 활용 html 반복 만들기

--- src/App.js
+++ src/App.js
@@ -26,9 +26,9 @@ function App() {
     "남자 코트 추천",
     "강남 우동맛집",
     "파이썬독학",
-    "좋아요1",
-    "좋아요2",
-    "좋아요3",
+    "좋아요",
+    "랄라라",
+    "모달창제목",
   ]);
   // a 는 state에 보관했던 자료 나옴
   // b 는 state 변경 도와주는 함수
@@ -38,6 +38,15 @@ function App() {
   // 자주 변경될거 같은 html 부분은 state 로 만들어 놓기 (날짜, 클래스네임 같은)
 
   let [따봉, 따봉변경] = useState(0);
+
+  // 모달창 만들기 useState
+  let [modal, setModal] = useState(false);
+
+  // map 사용법
+  [1, 2, 3].map(function (a) {
+    // console.log(a);
+    return "123321";
+  });
 
   return (
     <div className="App">
@@ -91,31 +100,94 @@ function App() {
         <p>2월 17일 발행</p>
         <p></p>
       </div>
-
       <div className="list">
         <h4>{글제목[2]}</h4>
-        <p>2월 17일 발행</p>
+        <p>2월 18일 발행</p>
       </div>
-
       <div className="list">
         <h4>{글제목2}</h4>
-        <p>2월 17일 발행</p>
+        <p>2월 19일 발행</p>
       </div>
 
       <div className="list">
         <h4>{글제목[3]}</h4>
-        <p>2월 17일 발행</p>
+        <p>2월 20일 발행</p>
       </div>
       <div className="list">
         <h4>{글제목[4]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{글제목[5]}</h4>
-        <p>2월 17일 발행</p>
+        <p>2월 21일 발행</p>
       </div>
 
-      <Modal></Modal>
+      {/* 모달창 띄워보기 */}
+      <h2 style={{ backgroundColor: "yellow" }}>모달창 띄워보기</h2>
+      <div className="list">
+        <h4
+          style={{ cursor: "pointer" }}
+          // onClick={() => {
+          //   if (modal == true) {
+          //     setModal(false);
+          //   } else {
+          //     setModal(true);
+          //   }
+          // }}
+          onClick={() => {
+            setModal(!modal);
+          }}
+        >
+          {글제목[5]}
+        </h4>
+        <p>2월 22일 발행</p>
+      </div>
+
+      {/* <Modal></Modal> */}
+
+      {modal == true ? <Modal /> : null}
+
+      {/* map()으로 같은 html 반복생성하는 법 */}
+      <h2 style={{ backgroundColor: "yellow" }}>
+        map()으로 같은 html 반복생성하는 법
+      </h2>
+      {글제목.map(function () {
+        return (
+          <div className="list">
+            <h4>{글제목[4]}</h4>
+            <p>2월 21일 발행</p>
+          </div>
+        );
+      })}
+
+      {/* 같은내용만 반복되지 않게 하기 */}
+      <h2 style={{ backgroundColor: "yellow" }}>
+        같은 내용으로 반복되지 않게 만들기
+      </h2>
+      {글제목.map(function (a) {
+        return (
+          <div className="list">
+            <h4>{a}</h4>
+            <p>5월 10일 발행</p>
+          </div>
+        );
+      })}
+
+      {/* 따봉표시 각자 변경되게하기 */}
+      <h2 style={{ backgroundColor: "yellow" }}>따봉표시 각자 변경되게하기</h2>
+      {글제목.map(function (a, i) {
+        return (
+          <div className="list" key={i}>
+            <h4>{글제목[i]}</h4>
+            <span
+              className="thumb_up"
+              onClick={() => {
+                따봉변경(따봉 + 1);
+              }}
+            >
+              👍
+            </span>
+            {따봉}
+            <p>2월 21일 발행</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
